48cd58e29168ac9db16a274b5dbcc0c45f958135
mahir31 <mahirajmal@live.com>
2020-12-17 11:50:14 +0000

    added features to fishing

--- cogs/fishing.py
+++ cogs/fishing.py
@@ -30,10 +30,10 @@ class fish(commands.Cog):
         if fisher:
             last_fished = fisher[0][3]
             can_fish = self.cooldown_calc(last_fished)
-            if bool(can_fish) == True:
+            if can_fish <= 0:
                 await self.go_fishing(ctx, fisher)
             else:
-                await ctx.send("imagine trying to go fishing while you're still on cooldown, couldn't be me.")
+                await ctx.send(f"imagine trying to go fishing while you're still on cooldown, couldn't be me. try again in {util.stringfromtimestamp(can_fish)} ⏰")
         else:
             db.go_fish(ctx.author.id, 0, 0, datetime.timestamp(datetime.now()), 0, 0)
             fisher = db.fisher_exists(ctx.author.id)
@@ -45,10 +45,10 @@ class fish(commands.Cog):
         if fisher:
             content = discord.Embed(title=f"Fisher {util.displayname(ctx.author)}'s profile", 
             colour=int('add8e6', 16))
-            content.description = f"""Total times fished: {fisher[0][1]}
-            Total Fish caught: {fisher[0][2]}
-            Last fished: {util.stringfromtimestamp(fisher[0][3])}
-            Experience points: {fisher[0][4]}"""
+            content.description = f"""🎣 Total times fished: {fisher[0][1]}
+            🐋 Total Fish caught: {fisher[0][2]}
+            ⏲️ Last fished: {util.stringfromtimestamp(self.cooldown_calc(fisher[0][3]))}
+            🌟 Experience points: {fisher[0][4]}"""
             await ctx.send(embed=content)
     
     # helper functions
@@ -69,25 +69,25 @@ class fish(commands.Cog):
                         last_fished = datetime.timestamp(datetime.now())
                         exp_points += 8
                         db.go_fish(fisher_id, times_fished, total_fish, last_fished, exp_points, coins)
-                        await ctx.send('Congratulations, you caught 1 fish and are awarded 8 xp!')
+                        await ctx.send('Congratulations, you caught 1 fish and are awarded 8 xp 🌟!')
                     else:
                         times_fished += 1
                         last_fished = datetime.timestamp(datetime.now())
                         exp_points += 4
                         db.go_fish(fisher_id, times_fished, total_fish, last_fished, exp_points, coins)
-                        await ctx.send('You tried your hardest to reel it in but the fish slipped away, you gain only 4 xp, better luck next time.')
+                        await ctx.send('You tried your hardest to reel it in but the fish slipped away, you gain only 4 xp ⭐, better luck next time.')
             except asyncio.TimeoutError:
                 times_fished += 1
                 last_fished = datetime.timestamp(datetime.now())
                 exp_points += 2
                 db.go_fish(fisher_id, times_fished, total_fish, last_fished, exp_points, coins)
-                await ctx.send('Oops, the fish escaped before you could reel it in, you gain 2 xp')
+                await ctx.send('Oops, the fish escaped before you could reel it in, you gain 2 xp 🎇')
         else:
             times_fished += 1
             last_fished = datetime.timestamp(datetime.now())
             exp_points += 2
             db.go_fish(fisher_id, times_fished, total_fish, last_fished, exp_points, coins)
-            await ctx.send('You cast your reel, but sadly no fish took the bait, you gain 1 xp try again later')
+            await ctx.send('You cast your reel, but sadly no fish took the bait, you gain 1 xp 🎇 try again later')
 
     def catch_check(ctx, payload):
         return payload.content == 'catch'
@@ -96,10 +96,7 @@ class fish(commands.Cog):
         cooldown = 1800
         can_fish = datetime.timestamp(datetime.now()) - last_fished
         can_fish = cooldown - can_fish
-        if can_fish <= 0:
-            return True
-        else:
-            return False
+        return can_fish
 
 # await ctx.send(f'fisher id: {fisher_id}, times fished: {times_fished}, total fish: {total_fish}, last fished: {last_fished}, exp points: {exp_points}, coins: {coins}')
 
