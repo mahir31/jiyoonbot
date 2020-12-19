9cbd1298b2b167cedbeb9e181f34c1ec4e910d65
mahir31 <mahirajmal@live.com>
2020-12-19 15:38:30 +0000

    added about section

---
+++ cogs/misc.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import logging
+
+colour = int('4eca58', 16)
+
+class misc(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logging.info('cog:misc.py connected')
+    
+    # commands
+
+    @commands.command()
+    async def about(self, ctx):
+        '''Bot information'''
+        content = discord.Embed(title='About:', colour=colour)
+        content.description = '''Name: Jiyoon Bot
+        Version: 1.02
+        Developer: doublesocks#3034'''
+        await ctx.send(embed=content)
+
+def setup(bot):
+    bot.add_cog(misc(bot))
