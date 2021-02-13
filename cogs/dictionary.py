0a8eecdc0a60c514aa5a5bafcff3fefe2317f401
mahir31 <mahirajmal@live.com>
2021-02-13 13:24:06 +0000

    new oxford dictionary function unformatted

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -1,9 +1,8 @@
 import discord
 from discord.ext import commands
 import logging
-from PyDictionary import PyDictionary
-
-dic = PyDictionary()
+from tools import ox_requests as ox
+import json
 
 class Dictionary(commands.Cog):
 
@@ -15,28 +14,14 @@ class Dictionary(commands.Cog):
         logging.info("cog: dictionary.py connected")
 
     # commands
-
     @commands.group(case_insensitive=True)
     async def dc(self, ctx):
         '''Dictionary commands'''
-    
+
     @dc.command(aliases=["df"])
     async def define(self, ctx, args):
-        '''type a word to find the definition'''
-        smth = dic.meaning(args)
-        await ctx.send(str(smth))
-    
-    @dc.command(aliasess=["an"])
-    async def antonym(self, ctx, args):
-        '''type a word to find words with the opposing definitions'''
-        smth = dic.antonym(args)
-        await ctx.send(str(smth))
-    
-    @dc.command(aliases=["sy"])
-    async def synonym(self, ctx, args):
-        '''type a word to find similar words'''
-        smth = dic.synonym(args)
-        await ctx.send(str(smth))
+        result = await ox.internal_call("entries", "en-gb", args)
+        await ctx.send(str(result))
 
 def setup(bot):
     bot.add_cog(Dictionary(bot))
---
+++ tools/ox_requests.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import os
+import asyncio
+from requests import exceptions
+import aiohttp
+
+from requests.models import HTTPError
+
+OXFORD_DICTIONARY_APPLICATION_ID = os.environ["OXFORD_DICTIONARY_APPLICATION_ID"]
+OXFORD_DICTIONARY_APPLICATION_KEY = os.environ["OXFORD_DICTIONARY_APPLICATION_KEY"]
+OXFORD_DICTIONARY_ENDPOINT = 'https://od-api.oxforddictionaries.com'
+
+async def internal_call(endpoint, source_lang, word_id):
+    url = f'{OXFORD_DICTIONARY_ENDPOINT}/api/v2/{endpoint}/{source_lang}/{word_id}'
+    headers = {
+        "app_id":OXFORD_DICTIONARY_APPLICATION_ID,
+        "app_key":OXFORD_DICTIONARY_APPLICATION_KEY
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            data = await response.json()
+            return data
