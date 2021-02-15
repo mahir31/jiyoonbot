346f61ce593845a08ab6e377e89784c31cff933b
mahir31 <mahirajmal@live.com>
2021-02-15 00:00:23 +0000

    something something

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -24,13 +24,20 @@ class Dictionary(commands.Cog):
         if 'error' in result:
             await ctx.send('it work big pog')
         else:
-            result = result['results'][0]['lexicalEntries'][0]
-            definitions = '\n'.join([definitions['definitions'][0] for definitions in result['entries'][0]['senses']])
-            synonyms = ', '.join(synonyms['text'] for synonyms in result['entries'][0]['senses'][0]['synonyms'])
-            audiofile = result['entries'][0]['pronunciations'][0]['audioFile']
-            await ctx.send(definitions)
+            await self.extraction_and_send(ctx, result['results'][0]['lexicalEntries'][0])
+
+    # helper functions
+
+    async def extraction_and_send(self, ctx, entry):
+        definitions = '\n'.join([definitions['definitions'][0] for definitions in entry['entries'][0]['senses']])
+        await ctx.send(definitions)
+        try:
+            synonyms = ', '.join(synonyms['text'] for synonyms in entry['entries'][0]['senses'][0]['synonyms'])
             await ctx.send(synonyms)
-            await ctx.send(audiofile)
+        except KeyError:
+            logging.info('no synonyms were found')
+        audiofile = entry['entries'][0]['pronunciations'][0]['audioFile']
+        await ctx.send(audiofile)
 
 def setup(bot):
     bot.add_cog(Dictionary(bot))
