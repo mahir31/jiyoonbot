2c1496924a5d26c1e4f607f226914007f8768278
mahir31 <mahirajmal@live.com>
2021-03-22 16:51:35 +0000

    changed events

--- cogs/events.py
+++ cogs/events.py
@@ -11,32 +11,28 @@ class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.statuses = [
-            ("watching", f"{len(self.bot.guilds)} servers"),
-            ("watching", "jiyoonbot.xyz")
+            (3, f"{len(self.bot.guilds)} servers"),
+            (3, "jiyoonbot.xyz")
         ]
-        self.activities = {"playing": 0, "streaming": 1, "listening": 2, "watching": 3}
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
         logging.info("cog: events.py connected")
-    
-    @commands.Cog.listener()
-    async def on_ready(self):
         self.status_loop.start()
 
     @tasks.loop(minutes=5.0)
     async def status_loop(self):
         try:
-            await self.next_status()
+            await self.change_status()
         except Exception as e:
             logging.error(f"{e}")
     
-    async def next_status(self):
+    async def change_status(self):
         new_status = random.randrange(0, len(self.statuses))
         new_status = self.statuses[new_status]
         await self.bot.change_presence(
             activity=discord.Activity(
-                type=discord.ActivityType(self.activities[new_status[0]]), name=new_status[1]
+                type=discord.ActivityType(new_status[0]), name=new_status[1]
             )
         )
 
