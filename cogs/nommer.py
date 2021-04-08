38b745aacc0ab9fb511193487e9739f22104c0ad
mahir31 <mahirajmal@live.com>
2021-04-08 13:43:22 +0100

    added class and made the cookies sorter more readable

--- cogs/nommer.py
+++ cogs/nommer.py
@@ -37,8 +37,8 @@ class Cookies(commands.Cog):
         """gifts cookies to mentioned users"""
         nommer = db.nommer_exists(ctx.author.id)
         if not nommer:
-            nommer = await self.instantiate_nommer(ctx, ctx.author.id)
-        if self.cooldown_calc(db.nommer_exists(ctx.author.id)[1]) < 0:
+            nommer = db.grab_cookies(ctx.author.id, datetime.timestamp(datetime.now() - timedelta(hours = 7)), 0, 0, 0, 0, 0)
+        if not self.cooldown_calc(db.nommer_exists(ctx.author.id)[1]) < 0:
             if user is None:
                 await self.cookie_types[random.choices(list(self.cookie_types.keys()), self.weights)[0]](ctx, ctx.author.id, None)
             else:
@@ -53,71 +53,60 @@ class Cookies(commands.Cog):
         return time_difference
 
     async def empty(self, ctx, gifter_id, giftee_id):
-        await self.cookies_sorter(ctx, db.nommer_exists(gifter_id), giftee_id, 0)
+        await self.cookies_sorter(ctx, Nommer(*db.nommer_exists(gifter_id)), giftee_id, 0)
 
     async def one(self, ctx, gifter_id, giftee_id):
-        await self.cookies_sorter(ctx, db.nommer_exists(gifter_id), giftee_id, 1)
+        await self.cookies_sorter(ctx, Nommer(*db.nommer_exists(gifter_id)), giftee_id, 1)
 
     async def some(self, ctx, gifter_id, giftee_id):
-        await self.cookies_sorter(ctx, db.nommer_exists(gifter_id), giftee_id, random.randint(2, 20))
+        await self.cookies_sorter(ctx, Nommer(*db.nommer_exists(gifter_id)), giftee_id, random.randint(2, 20))
 
     async def nom(self, ctx, gifter_id, giftee_id):
-        await self.cookies_sorter(ctx, db.nommer_exists(gifter_id), giftee_id, random.randint(21, 60))
+        await self.cookies_sorter(ctx, Nommer(*db.nommer_exists(gifter_id)), giftee_id, random.randint(21, 60))
     
     async def cookies_sorter(self, ctx, nommer, giftee_id, increment):
+        giftee = db.nommer_exists(giftee_id)
         if giftee_id:
             db.grab_cookies(
-                nommer[0],
+                nommer.nommer_id,
                 datetime.timestamp(datetime.now()),
-                nommer[2],
-                nommer[3] + increment,
-                nommer[4] + increment,
-                nommer[5] + 1,
-                nommer[6]
+                nommer.total_cookies,
+                nommer.total_cookies_grabbed + increment,
+                nommer.total_cookies_gifted + increment,
+                nommer.total_grab_attempts + 1,
+                nommer.total_cookies_received
             )
-            giftee = db.nommer_exists(giftee_id)
             if giftee:
+                giftee = Nommer(*giftee)
                 db.grab_cookies(
-                    giftee[0],
-                    giftee[1],
-                    giftee[2] + increment,
-                    giftee[3],
-                    giftee[4],
-                    giftee[5],
-                    giftee[6] + increment
+                    giftee.nommer_id,
+                    giftee.last_grabbed,
+                    giftee.total_cookies + increment,
+                    giftee.total_cookies_grabbed,
+                    giftee.total_cookies_gifted,
+                    giftee.total_grab_attempts,
+                    giftee.total_cookies_received + increment
                 )
             else:
-                db.grab_cookies(
-                    giftee_id, 
-                    datetime.timestamp(datetime.now() - timedelta(hours = 6)), 
-                    increment, 
-                    0, 
-                    0, 
-                    0, 
-                    increment
-                )
+                db.grab_cookies(giftee_id, datetime.timestamp(datetime.now() - timedelta(hours = 6)), increment, 0, 0, 0, increment)
             if increment == 0:
                 await ctx.send(f"{util.displayname(await self.bot.fetch_user(ctx.author.id))} went to grab some cookies but didn't get any\N{Broken Heart}")
             else:
-                await ctx.send(f'\N{Cookie}{util.displayname(await self.bot.fetch_user(nommer[0]))} grabbed {increment} cookie(s) and gifted it to {util.displayname(await self.bot.fetch_user(giftee_id))}\N{Sparkling Heart}')
+                await ctx.send(f'\N{Cookie}{util.displayname(await self.bot.fetch_user(ctx.author.id))} grabbed {increment} cookie(s) and gifted it to {util.displayname(await self.bot.fetch_user(giftee_id))}\N{Sparkling Heart}')
         else:
             db.grab_cookies(
-                nommer[0], 
+                nommer.nommer_id, 
                 datetime.timestamp(datetime.now()), 
-                nommer[2] + increment, 
-                nommer[3] + increment, 
-                nommer[4], 
-                nommer[5] + increment,
-                nommer[6]
+                nommer.total_cookies + increment, 
+                nommer.total_cookies_grabbed + increment, 
+                nommer.total_cookies_gifted,
+                nommer.total_grab_attempts + increment,
+                nommer.total_cookies_received
             )
             if increment == 0:
                 await ctx.send(f"{util.displayname(await self.bot.fetch_user(ctx.author.id))} went to grab some cookies but didn't get any\N{Broken Heart}")
             else:
-                await ctx.send(f'\N{Cookie}{util.displayname(await self.bot.fetch_user(nommer[0]))} grabbed {increment} cookie(s)\N{Sparkling Heart}')
-    
-    async def instantiate_nommer(self, ctx, nommer_id):
-        db.grab_cookies(nommer_id, datetime.timestamp(datetime.now() - timedelta(hours = 7)), 0, 0, 0, 0, 0)
-        return
+                await ctx.send(f'\N{Cookie}{util.displayname(await self.bot.fetch_user(ctx.author.id))} grabbed {increment} cookie(s)\N{Sparkling Heart}')
     
 def setup(bot):
     bot.add_cog(Cookies(bot))
